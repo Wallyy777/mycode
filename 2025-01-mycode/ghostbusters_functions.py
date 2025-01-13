c0261755ab951c3e9ea3c92e64dfefe56cf987b9
Mona Lisa <MonaLisa@example.com>
2025-01-13 19:19:07 +0000

    backing up work

---
+++ 2025-01-mycode/ghostbusters_functions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Ghostbusters Lab | Step 4"""
+
+def report_ghost_sighting(ghost_name, location="New York City"):
+    """Prints details about the ghost sighting with a default location."""
+    print(f"{ghost_name} has been sighted at {location}! Who you gonna call?")
+
+# Function calls
+report_ghost_sighting("Slimer", "Hotel Sedgewick")  # Valid call
+report_ghost_sighting("Stay Puft")  # Valid call, 'location' defaults to "New York City"
+if __name__ == "maim":
+    main()
